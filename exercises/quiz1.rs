89dc7be9b25d70a59300c1a8d4f5a08387ef1871
w0r1d <83357578+cs-w0r1d@users.noreply.github.com>
2024-04-12 13:31:21 +0000

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,19 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+ fn calculate_price_of_apples(quantity: i32) -> i32{
+    let price_per_apple = if quantity > 40 {
+        1
+    } else {
+        2
+    };
+
+    quantity * price_per_apple
+ }
+
 
 // Don't modify this function!
 #[test]
